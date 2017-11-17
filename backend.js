653307f125cdd61da47a38035ea44f170aebed8b
kellykeller1213 <keller.f.peter@gmail.com>
2017-11-17 12:15:32 +0100

    feat(licence_plate): two endpoints created, still have to work on it

---
+++ backend.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+app.use(express.json());
+
+app.use(express.static('/assets/frontend'));
+app.use('/assets', express.static('./assets'));
+
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ridata94',
+    database: 'licence_plates'
+    });
+
+connection.connect(function(err){
+    if (err) {
+        console.log("Cannot connect to database");
+    } else { 
+        console.log("Connection estabilished");
+}});
+
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/assets/index.html');
+});
+
+app.get('/search?', function(request, response) {
+    console.log(request.query.q);
+    if (request.query.q) {
+        var queryString = `SELECT * FROM licence_plates WHERE plate = '${request.query.q}'`;
+    } else {
+        var queryString = 'SELECT * FROM licence_plates;'
+    };
+    connection.query(queryString, function(err, result) {
+    response.send(result);
+});
+});
+
+app.get('/search/:brand', function(request, response) {
+    console.log(request.params.brand)
+    var dataAccess = 'SELECT * FROM licence_plates;'
+    if (dataAccess.car_brand == request.params.brand) {
+        var queryString = `SELECT * FROM licence_plates WHERE car_brand = '${request.params.brand}'`
+    } else {
+        var queryString = 'SELECT * FROM licence_plates;'
+    };
+    connection.query(queryString, function(err, result) {
+    response.send(result);
+    });
+});
+
+
+app.listen(8080,  () => console.log('Running'));
